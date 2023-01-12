3ffffe9c0e9ede545a1bcba10fcf0ea4fa22f13f
yeong96 <69592002+doyeong1996@users.noreply.github.com>
2023-01-12 23:36:17 +0900

    my-momentum#3

--- js/todo.js
+++ js/todo.js
@@ -7,10 +7,11 @@ const todoList = document.querySelector("#todo-list");
 
 const toDos = []; //localStorage에 저장할 배열을 변수로저장
 
+const TODOS_KEY = "todos";
 function saveTodos() {
   // toDos의 배열안에있는 값들을 localStorage에 저장
   // localStorage.setItem 저장하지만 값은 배열의 값으로 저장이아닌 단순히 문자열로 저장됨
-  localStorage.setItem("todos", JSON.stringify(toDos)); // 배열에 저장된값을 JSON.stringify(변수) 작성해주면 배열그대로 값을 저장해준다
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos)); // 배열에 저장된값을 JSON.stringify(변수) 작성해주면 배열그대로 값을 저장해준다
 }
 
 function todoDelete(event) {
@@ -41,3 +42,10 @@ function todoInputSubmit(event) {
   saveTodos();
 }
 todoForm.addEventListener("submit", todoInputSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if (savedToDos !== null) {
+  const parsadToDos = JSON.parse(savedToDos);
+  parsadToDos.forEach((item) => console.log("this is ", item));
+}
