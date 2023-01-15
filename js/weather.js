39035300466f92c41db9a89a9ad5fb9507e810cf
yeong96 <69592002+doyeong1996@users.noreply.github.com>
2023-01-15 23:35:20 +0900

    my-momentum#5

--- js/weather.js
+++ js/weather.js
@@ -1,10 +1,17 @@
-const API_KEY = "f9f325bfc90237bef059dbbbf4c050d2 ";
+const API_KEY = "f9f325bfc90237bef059dbbbf4c050d2";
 
 function onGeoOk(position) {
   const lat = position.coords.latitude; // 위도
-  const lng = position.coords.longitude; // 경도
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}`;
-  console.log(url);
+  const ion = position.coords.longitude; // 경도
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${ion}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((date) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      city.innerText = date.name;
+      weather.innerText = `${date.weather[0].main} / ${date.main.temp}`;
+    });
 }
 function onError() {
   alert("Can't find you. No weather for you.");
